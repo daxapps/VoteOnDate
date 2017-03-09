088b1c019dbd7e9f0bb91c63ee3f60542d5a2abf
daxapps <daxapps777@gmail.com>
2017-03-08 18:48:15 -0600

    Select possible dates

--- MessagesExtension/EventViewController.swift
+++ MessagesExtension/EventViewController.swift
@@ -13,6 +13,11 @@ class EventViewController: UIViewController, UITableViewDataSource, UITableViewD
     @IBOutlet weak var tableView: UITableView!
     @IBOutlet weak var datePicker: UIDatePicker!
     
+    var dates = [Date]()
+    var allVotes = [Int]()
+    var ourVotes = [Int]()
+    
+    
     override func viewDidLoad() {
         super.viewDidLoad()
 
@@ -24,12 +29,31 @@ class EventViewController: UIViewController, UITableViewDataSource, UITableViewD
     }
     
     func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
-        return 1
+        return dates.count
     }
     
     func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        
+        //1 - dequeue a cell
         let cell = tableView.dequeueReusableCell(withIdentifier: "Date", for: indexPath)
-        cell.textLabel?.text = "Date goes here"
+        
+        //2 - pull out the corresponding date and format it neatly
+        let date = dates[indexPath.row]
+        cell.textLabel?.text = DateFormatter.localizedString(from: date, dateStyle: .long, timeStyle: .short)
+        
+        // add a checkmark if we voted for this date
+        if ourVotes[indexPath.row] == 1 {
+            cell.accessoryType = .checkmark
+        } else {
+            cell.accessoryType = .none
+        }
+        
+        // add a vote count if other people vited for this date
+        if allVotes[indexPath.row] > 0 {
+            cell.detailTextLabel?.text = "Votes: \(allVotes[indexPath.row])"
+        } else {
+            cell.detailTextLabel?.text = ""
+        }
         
         return cell
     }
@@ -38,8 +62,38 @@ class EventViewController: UIViewController, UITableViewDataSource, UITableViewD
     }
     
     @IBAction func addDate(_ sender: Any) {
+        
+        //1 - add to the arrays
+        dates.append(datePicker.date)
+        allVotes.append(0)
+        ourVotes.append(1)
+        
+        //2 - insert a row in the table using animation
+        let newIndexPath = IndexPath(row: dates.count - 1, section: 0)
+        tableView.insertRows(at: [newIndexPath], with: .automatic)
+        
+        //3 - scroll the new row into view
+        tableView.scrollToRow(at: newIndexPath, at: .bottom, animated: true)
+        
+        //4 - flash the scroll bars so the user knows something has changed
+        tableView.flashScrollIndicators()
+        
     }
     
+    func tableView(_ tableView: UITableView, didSelectRowAt indexPath: IndexPath) {
+        
+        tableView.deselectRow(at: indexPath, animated: true)
+        
+        if let cell = tableView.cellForRow(at: indexPath) {
+            if cell.accessoryType == .checkmark {
+                cell.accessoryType = .none
+                ourVotes[indexPath.row] = 0
+            } else {
+                cell.accessoryType = .checkmark
+                ourVotes[indexPath.row] = 1
+            }
+        }
+    }
     
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
